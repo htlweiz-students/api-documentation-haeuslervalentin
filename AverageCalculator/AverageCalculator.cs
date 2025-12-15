1b16191172aec62f16fe7d38fd1d3c5b905d6639
Student <foobar@sr.htlweiz.at>
2025-12-15 10:44:57 +0100

    AverageCal class

---
+++ AverageCalculator/AverageCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AverageCalculator;
+
+public class AverageCalculator
+{
+    private List<double> values;
+
+    public AverageCalculator()
+    {
+        values = new List<double>();
+    }
+
+    public void AddValue(double new_value)
+    {
+        values.Add(new_value);
+    }
+
+    public void AddValues(IEnumerable<double> new_values)
+    {
+        values.AddRange(new_values);
+    }
+
+    public double getAverage()
+    {
+       if(values.Any()) 
+       {
+            //Eigene Exception class
+       }
+        double avg = values.Average();
+        return avg; 
+    }
+
+    public IReadOnlyList<double> getElements()
+    {
+        return values.AsReadOnly();
+    }
+}
